de421423e6201981de1711d05ab5780883c0456e
Mahendra Hirapra <hir.mahendra@yahoo.com>
2017-04-19 19:13:53 +0530

    update updateCurrencyAction.js

--- site/actions/updateCurrencyAction.js
+++ site/actions/updateCurrencyAction.js
@@ -4,7 +4,6 @@ import _ from 'lodash';
 export const updateCurrency = (currency) => ({ 
     type: types.UPDATE_CURRENCY, 
     currency : currency,
-    dateModified : _.now(),
     midprice : (currency.lastChangeBid + currency.lastChangeAsk) /2
 });
 
